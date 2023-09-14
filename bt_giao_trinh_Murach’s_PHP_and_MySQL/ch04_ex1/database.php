18ef623aacb6c768a58bedcc8141422cd1c747d4
Thai Hua <huavietthai299@gmail.com>
2023-09-14 16:17:48 +0700

    ch04_ex1

---
+++ bt_giao_trinh_Murach’s_PHP_and_MySQL/ch04_ex1/database.php
@@ -0,0 +1,16 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
+    //$username = 'mgs_user';
+    //$password = 'pa55word';
+	
+	$username = 'root';
+    $password = '';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+?>
